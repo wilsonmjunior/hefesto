a671ab428c75a49fbcdf6f1053d1c8c6d8ffda10
Wilson Junior <wilson.junior@merx.tech>
2023-04-01 12:42:21 -0300

    fix: adjust header

--- src/components/common/Layout/Header.tsx
+++ src/components/common/Layout/Header.tsx
@@ -1,26 +1,16 @@
-import Link from "next/link";
+import { Breadcrumbs } from "../Breadcrumbs";
 
 interface HeaderProps {
   title: string;
+  paths: string[];
 }
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, paths }: HeaderProps) {
   return (
     <div className="items-center my-10">
       <h1>{title}</h1>
 
-      <nav aria-label="breadcrumb" className="mt-2">
-        <ol className="flex leading-none text-gray-400 font-medium divide-x divide-gray-400">
-          <li className="pr-2">
-            <Link href="#">
-              Home
-            </Link>
-          </li>
-          <li className="px-2 text-gray-900">
-            Repositories
-          </li>
-        </ol>
-      </nav>
+      <Breadcrumbs paths={paths} />
     </div>
   )
 }
