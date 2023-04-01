c89f766cb23e4f9b94535ee1d238f3fb5d9f08a1
Wilson Junior <wilson.junior@merx.tech>
2023-04-01 12:43:52 -0300

    fix: import dropdown

--- src/app/repositories/page.tsx
+++ src/app/repositories/page.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-import { Header, Table } from "@/components";
+import { Dropdown, Header, Table } from "@/components";
 import { columns } from "./columns";
 
 const data = [
@@ -20,10 +18,6 @@ const data = [
   },
 ];
 
-const TesteIcon = () => (
-  <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-)
-
 export default function Repositories() {
   return (
     <div className="container mx-auto my-10">
@@ -33,6 +27,29 @@ export default function Repositories() {
         data={data} 
         columns={columns}
       />
+
+      <div className="flex justify-end mt-10">
+        <Dropdown 
+          data={[
+            { title: "Item 1" },
+            { title: "Item 2" }
+          ]}
+          startIcon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+
+          }
+          label="Options"
+        />
+      </div>
     </div>
   )
 }
