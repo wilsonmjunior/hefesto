a7acc8222de051a86e057ebaaff37062458b18a4
Wilson Junior <wilson.junior@merx.tech>
2023-04-02 22:17:49 -0300

    feat: create datepicker component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 export { ActionMenu } from "./common/ActionMenu"
 export { Button } from "./common/Button"
 export { Breadcrumbs } from "./common/Breadcrumbs"
-export { Dropdown } from './common/Dropdown';
+export { DatePicker } from './common/DatePicker'
+export { Dropdown } from './common/Dropdown'
 export type { DropdownItemProps, DropdownProps } from "./common/Dropdown"
 export { IconButton } from "./common/IconButton"
 export { Table } from "./common/Table"
