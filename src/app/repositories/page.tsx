176f339917acf7aabc33b59cea97cad9f31192c1
Wilson Junior <wilson.junior@merx.tech>
2023-05-01 22:22:04 -0300

    fix: get list repositories

--- src/app/repositories/page.tsx
+++ src/app/repositories/page.tsx
@@ -1,40 +1,27 @@
-import { Header, Table } from "@/components";
-import { columns } from "./columns";
+import { getSearchIssues } from "@/api/repositories"
+import { RepoList } from "@/components/pages/repositories/List"
+import { getPageProps, SearchParamsType } from "@/utils/pagination";
 
-import { FormRepository } from "@/components/pages/repositories/FormRepository";
+type RepositoriesProps = {
+  searchParams: SearchParamsType;
+}
 
-const data = [
-  {
-    id: 1,
-    name: "Product 1",
-    description: "This is a product",
-    producers: ["Producer 1", "Producer 2"],
-    createdAt: "2022-03-29",
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    description: "This is another product",
-    producers: ["Producer 3", "Producer 4"],
-    createdAt: "2022-03-28",
-  },
-];
+export default async function Repositories({ searchParams }: RepositoriesProps) {
+  const repos = await getSearchIssues('')
 
-export default function Repositories() {
+  const { page, size } = getPageProps({
+    searchParams,
+  })
+
   return (
-    <div className="container mx-auto my-10">
-      <Header title="Repositories" paths={['Home', 'Repositories']} />
-
-      <div className="container mx-auto p-7 rounded-lg border">
-        <Table
-          data={data} 
-          columns={columns}
-        />
-      </div>
-
-      <div className="mt-10">
-        <FormRepository />
-      </div>
-    </div>
+    <RepoList
+      items={repos}
+      paginationProps={{
+        currentPage: page,
+        pathBase: 'repositories',
+        rowsPerPage: size,
+        totalPages: 10,
+      }}
+    />
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
-export { ActionMenu } from "./common/ActionMenu"
 export { Button } from "./common/Button"
 export { Breadcrumbs } from "./common/Breadcrumbs"
 export { DatePicker } from './common/DatePicker'
 export { Dropdown } from './common/Dropdown'
 export type { DropdownItemProps, DropdownProps } from "./common/Dropdown"
 export { IconButton } from "./common/IconButton"
-export { Table } from "./common/Table"
+export { Table } from "./common/Table/v2"
 export { Header } from "./common/Layout"
