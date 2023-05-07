55ee053306507035255d9328e7a001cb03f9c503
Wilson Junior <wilson.junior@merx.tech>
2023-05-07 12:06:44 -0300

    fix: form

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,20 +1,52 @@
+type FormData = {
+  name: string;
+  age: string;
+};
+
 export default function Page() {
-  async function onSave() {
+  async function onSave(formData: unknown[]) {
     'use server';
-    console.log('onSave')
+    console.log('onSave', formData)
   }
 
   return (
     <form
       action={onSave}
-      className="border rounded-xl p-10 m-10"
+      className="grid
+        grid-cols-12
+        gap-2
+        border
+        rounded-xl
+        p-10
+        m-10
+        shadow"
     >
-      <button
-        type="submit"
-        className="p-2 rounded-lg text-white bg-gray-600 hover:bg-gray-400 "
-      >
-        Save
-      </button>
+      <div className="col-span-4 flex flex-col">
+        <label htmlFor="label-name">Name</label>
+        <input name="name" className="mt-1 py-3 px-5 border rounded-md" type="text" />
+      </div>
+
+      <div className="col-span-4 flex flex-col">
+        <label htmlFor="label-age">Age</label>
+        <input name="age" className="mt-1 py-3 px-5 border rounded-md" type="text" />
+      </div>
+
+      <div className="col-span-3 self-end">
+        <button
+          type="submit"
+          className="w-full
+            py-3
+            px-5
+            rounded-lg
+            bg-gray-600
+            text-white
+            hover:bg-gray-500
+            transition-all
+            ease-linear"
+        >
+          Save
+        </button>
+      </div>
     </form>
   );
 }
